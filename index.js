1bc41ab69e9cd6291f59b39fdc65db416de64ad9
Lucas LE RAY <lucas.a.leray@gmail.com>
2019-12-08 02:06:36 -0800

    feat: rebuild db

--- index.js
+++ index.js
@@ -3,12 +3,40 @@
 const dotenv = require('dotenv')
 const AWS = require('aws-sdk')
 const Twit = require('twit')
+const minimist = require('minimist')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
+const args = minimist(process.argv.slice(2), {
+  alias: {
+    h: 'help',
+    s: 'subject',
+  },
+})
+
+if (!args.s) {
+  console.error('No subject.')
+  process.exit(1)
+}
+
+const dateObj = new Date()
+const month = dateObj.getUTCMonth() + 1
+const day = dateObj.getUTCDate()
+const year = dateObj.getUTCFullYear()
+const date = `${month}-${day}-${year}`
 const adapter = new FileSync('db.json')
 const db = low(adapter)
-db.defaults({ sentiments: [], sinceId: 0 }).write()
+db.defaults({
+  [args.s]: {
+    [date]: {
+      positive: 0,
+      negative: 0,
+      neutral: 0,
+      count: 0,
+    },
+    sinceId: 0,
+  },
+}).write()
 
 dotenv.config()
 
@@ -51,21 +79,52 @@ async function getTweets(q, sinceId) {
 
 (async () => {
   const sentiments = []
-  const sinceId = db.get('sinceId').value()
-  const tweets = await getTweets('Donald Trump', sinceId)
-  db.update('sinceId', () => tweets[0].id).write()
+  const sinceId = db
+    .get(args.s)
+    .get('sinceId')
+    .value()
+  const tweets = await getTweets(args.s, sinceId)
+  db.get(args.s)
+    .update('sinceId', () => tweets[0].id)
+    .write()
   for (let i = 0; i < tweets.length; i += 1) {
     sentiments.push(detectSentiment(tweets[i].text, 'en', tweets[i].id))
   }
   const res = await Promise.all(sentiments)
-  db.get('sentiments')
-    .push(
-      ...res.map(r => ({
-        id: r.id,
-        positive: r.SentimentScore.Positive,
-        negative: r.SentimentScore.Negative,
-        neutral: r.SentimentScore.Neutral,
-      })),
+  let reduced = res
+    .map(sentiment => sentiment.SentimentScore)
+    .reduce((acc, c) => ({
+      Positive: acc.Positive + c.Positive,
+      Negative: acc.Negative + c.Negative,
+      Neutral: acc.Neutral + c.Neutral,
+    }))
+  reduced = {
+    positive: reduced.Positive,
+    negative: reduced.Negative,
+    neutral: reduced.Neutral,
+  }
+  const previousCount = db
+    .get(args.s)
+    .get(date)
+    .get('count')
+    .value()
+  db.get(args.s)
+    .get(date)
+    .update(
+      'positive',
+      positive => (positive * previousCount + reduced.positive)
+        / (res.length + previousCount),
     )
+    .update(
+      'negative',
+      negative => (negative * previousCount + reduced.negative)
+        / (res.length + previousCount),
+    )
+    .update(
+      'neutral',
+      neutral => (neutral * previousCount + reduced.neutral)
+        / (res.length + previousCount),
+    )
+    .update('count', count => count + res.length)
     .write()
 })()
